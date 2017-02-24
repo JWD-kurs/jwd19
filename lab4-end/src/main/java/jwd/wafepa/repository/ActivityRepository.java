494c8cdddca101bc649ed77496628e4e4b3fd775
Zeljko Vukovic <zeljko.vukovic.ns@gmail.com>
2017-02-24 20:58:27 +0100

    lab4-end

---
+++ lab4-end/src/main/java/jwd/wafepa/repository/ActivityRepository.java
@@ -0,0 +1,20 @@
+package jwd.wafepa.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import jwd.wafepa.model.Activity;
+
+@Repository
+public interface ActivityRepository
+	extends JpaRepository<Activity, Long> {
+
+//	@Query("select a from Activity a where a.name like :name")
+//	List<Activity> findByName(@Param("name") String name);
+	
+	List<Activity> findByName(String name);
+	
+	List<Activity> findByNameContaining(String name);
+}
